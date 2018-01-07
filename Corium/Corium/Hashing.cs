c6cacbe3ce09e7c5e93f4410a489a594ceec765e
BitSorcerer <ss>
2018-01-07 19:13:23 +0200

    Init

---
+++ Corium/Corium/Hashing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Corium{
+    public static class Hashing{
+        public static string ToHex(this byte[] bytes){
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        public static byte[] GetBytes<T>(T obj){
+            int size = Marshal.SizeOf(obj);
+            byte[] bytes = new byte[size];
+
+            GCHandle h = default(GCHandle);
+            try{
+                h = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+                Marshal.StructureToPtr<T>(obj, h.AddrOfPinnedObject(), false);
+            }
+            finally{
+                if (h.IsAllocated){
+                    h.Free();
+                }
+            }
+            return bytes;
+        }
+    }
+}
---
+++ Corium/Corium/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Corium;
+
+namespace Corium{
+    class Program{
+        //Testing...
+        static void Main(string[] args){
+            BlockChain.NewBlock("11111211", "Testasadsadus testausss");
+
+            string[] dataArray = new string[] { "a", "b", "c", "d", "e" };
+
+            for (int i = 0; i < dataArray.Length; i++){
+                BlockChain.Block block = BlockChain.blocks[BlockChain.blocks.Count() - 1];
+                Console.WriteLine(block.hash);
+                BlockChain.NewBlock(block.hash, dataArray[i]);
+            }
+        }
+    }
+}
